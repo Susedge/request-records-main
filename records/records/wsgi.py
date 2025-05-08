3c2666d4b048ddb83d6b6a93649324e209f9203a
Susedge <166993665+Susedge@users.noreply.github.com>
2025-05-09 00:20:47 +0800

    Test

--- records/records/wsgi.py
+++ records/records/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'records.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'records.records.settings')
 
 application = get_wsgi_application()
