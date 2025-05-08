87bd2f641d225760c37c076288f5818334fa6bfb
Susedge <166993665+Susedge@users.noreply.github.com>
2025-05-09 00:34:46 +0800

    Commit

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,13 @@
-from records.records.wsgi import application
+import os
+import sys
 
-# This allows "gunicorn wsgi" to work
+# Add the parent directory to the Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# Add the project directory to the Python path
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'records'))
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'records.records.settings')
+application = get_wsgi_application()
